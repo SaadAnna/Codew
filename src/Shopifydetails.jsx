4db7e4a0807938b26904258cd2ac2cc010904f91
saad <saadannadev@gmail.com>
2024-10-22 12:13:42 +0200

    modified all pages in the project

--- src/Shopifydetails.jsx
+++ src/Shopifydetails.jsx
@@ -245,17 +245,17 @@ Dedicated assistance for updates<br /> maintenance, and future enhancements.
           <div
         data-aos="fade-up"
         data-aos-duration="900"
-        className="  block mt-56 text-start ml-7 lg:flex lg:justify-between lg:items-center lg:text-center lg:mt-72 lg:ml-14"
+        className="  block mt-56 text-start ml-8 md:flex md:justify-between md:items-center md:text-center lg:mt-72 lg:ml-14"
       >
-        <div className=" mt-10  lg:block lg:text-start  lg:justify-start lg:items-start">
+        <div className=" mt-9  flex flex-col text-center justify-start items-start">
           <h1 className="text-2xl font-Text font-medium  text-black">Codew</h1>
-          <p className="text-neutral-600 text-base font-Text mt-4">
+          <p className="text-neutral-600 text-start text-base font-Text mt-4">
             make your business more famous by <br /> designing a wonderful and
             attractive <br />
             website with codew.
           </p>
           <div className="flex gap-2 mt-3">
-            <a href="#">
+            <a target="_blank" href="https://x.com/CodewFront_end">
               {" "}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -266,7 +266,7 @@ Dedicated assistance for updates<br /> maintenance, and future enhancements.
                 <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.066 9.645c.183 4.04-2.83 8.544-8.164 8.544-1.622 0-3.131-.476-4.402-1.291 1.524.18 3.045-.244 4.252-1.189-1.256-.023-2.317-.854-2.684-1.995.451.086.895.061 1.298-.049-1.381-.278-2.335-1.522-2.304-2.853.388.215.83.344 1.301.359-1.279-.855-1.641-2.544-.889-3.835 1.416 1.738 3.533 2.881 5.92 3.001-.419-1.796.944-3.527 2.799-3.527.825 0 1.572.349 2.096.907.654-.128 1.27-.368 1.824-.697-.215.671-.67 1.233-1.263 1.589.581-.07 1.135-.224 1.649-.453-.384.578-.87 1.084-1.433 1.489z" />
               </svg>
             </a>
-            <a href="#">
+            <a target="_blank" href="https://www.instagram.com/codew_dev/">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="35"
@@ -276,7 +276,7 @@ Dedicated assistance for updates<br /> maintenance, and future enhancements.
                 <path d="M14.829 6.302c-.738-.034-.96-.04-2.829-.04s-2.09.007-2.828.04c-1.899.087-2.783.986-2.87 2.87-.033.738-.041.959-.041 2.828s.008 2.09.041 2.829c.087 1.879.967 2.783 2.87 2.87.737.033.959.041 2.828.041 1.87 0 2.091-.007 2.829-.041 1.899-.086 2.782-.988 2.87-2.87.033-.738.04-.96.04-2.829s-.007-2.09-.04-2.828c-.088-1.883-.973-2.783-2.87-2.87zm-2.829 9.293c-1.985 0-3.595-1.609-3.595-3.595 0-1.985 1.61-3.594 3.595-3.594s3.595 1.609 3.595 3.594c0 1.985-1.61 3.595-3.595 3.595zm3.737-6.491c-.464 0-.84-.376-.84-.84 0-.464.376-.84.84-.84.464 0 .84.376.84.84 0 .463-.376.84-.84.84zm-1.404 2.896c0 1.289-1.045 2.333-2.333 2.333s-2.333-1.044-2.333-2.333c0-1.289 1.045-2.333 2.333-2.333s2.333 1.044 2.333 2.333zm-2.333-12c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.958 14.886c-.115 2.545-1.532 3.955-4.071 4.072-.747.034-.986.042-2.887.042s-2.139-.008-2.886-.042c-2.544-.117-3.955-1.529-4.072-4.072-.034-.746-.042-.985-.042-2.886 0-1.901.008-2.139.042-2.886.117-2.544 1.529-3.955 4.072-4.071.747-.035.985-.043 2.886-.043s2.14.008 2.887.043c2.545.117 3.957 1.532 4.071 4.071.034.747.042.985.042 2.886 0 1.901-.008 2.14-.042 2.886z" />
               </svg>
             </a>
-            <a href="#">
+            <a target="_blank" href="https://www.youtube.com/@codew_dev">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="35"
@@ -288,7 +288,7 @@ Dedicated assistance for updates<br /> maintenance, and future enhancements.
             </a>
           </div>
         </div>
-        <div className=" mt-16 lg:block lg:text-start lg:justify-start lg:items-start">
+        <div className=" mt-16 flex flex-col text-start justify-start items-start">
           <h1 className="text-xl font-Text font-medium  text-black">
             Quick Links
           </h1>
@@ -325,7 +325,7 @@ Dedicated assistance for updates<br /> maintenance, and future enhancements.
             </li>
           </ul>
         </div>
-        <div className=" mt-14  mr-14 lg:block lg:text-start lg:mr-14 lg:justify-start lg:items-start">
+        <div className=" mt-16  mr-14 flex flex-col text-start  justify-start items-start">
           <h1 className="text-xl font-Text font-medium  text-black">Contact</h1>
           <ul>
             <li>
@@ -337,19 +337,22 @@ Dedicated assistance for updates<br /> maintenance, and future enhancements.
                 Twitter
               </a>
               <a
-                href="#"
+                target="_blank"
+                href="https://www.instagram.com/codew_dev/"
                 className=" text-neutral-600 hover:text-black  text-base font-Text mt-4 block lg:text-neutral-600 lg:text-base lg:font-Text lg:mt-4 lg:block lg:hover:text-black"
               >
                 Instagram
               </a>
               <a
-                href="#"
+                target="_blank"
+                href="https://github.com/Codew-dev"
                 className=" text-neutral-600 hover:text-black  text-base font-Text mt-4 block lg:text-neutral-600 lg:text-base lg:font-Text lg:mt-4 lg:block lg:hover:text-black"
               >
                 Git Hub
               </a>
               <a
-                href="#"
+                target="_blank"
+                href="https://www.youtube.com/@codew_dev"
                 className=" text-neutral-600 hover:text-black  text-base font-Text mt-4 block lg:text-neutral-600 lg:text-base lg:font-Text lg:mt-4 lg:block lg:hover:text-black"
               >
                 Youtube
@@ -359,7 +362,7 @@ Dedicated assistance for updates<br /> maintenance, and future enhancements.
         </div>
       </div>
       <hr className="w-11/12 mt-14 bg-neutral-200  m-auto " />
-      <div className=" mt-14   text-center  block justify-start items-start lg:pb-10 lg:flex lg:justify-between lg:items-start lg:mt-14 lg:m-auto">
+      <div className=" mt-14 pb-10   text-center  block justify-start items-start lg:pb-10 lg:flex lg:justify-between lg:items-start lg:mt-14 lg:m-auto">
         <h3 className=" text-neutral-600 text-base font-Text block lg:ml-14 lg:text-neutral-600 lg:text-base lg:font-Text ">
           Build By Codew.
         </h3>
